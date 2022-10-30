c873e6776e8159a17c311fed00b73f03af83aad8
PauladaHR <pemeduardo1@hotmail.com>
2022-10-30 00:13:14 -0300

    Modificado para o minimap circular.

--- resources/[scripts]/vrp_hud/client-side/map.lua
+++ resources/[scripts]/vrp_hud/client-side/map.lua
@@ -6,8 +6,14 @@ CreateThread(function()
 		AddReplaceTexture("platform:/textures/graphics","radarmasksm","circlemap","radarmasksm")
 
 		SetMinimapClipType(1)
-		SetMinimapComponentPosition("minimap","L","B",-0.0045,0.002-0.025,0.150,0.188888)
-		SetMinimapComponentPosition("minimap_mask","L","B",0.020,0.032-0.025,0.111,0.159)
-		SetMinimapComponentPosition("minimap_blur","L","B",-0.025,0.022-0.025,0.266,0.237)
+		SetMinimapComponentPosition("minimap","L","B",0.0,-0.015,0.129,0.24)
+		SetMinimapComponentPosition("minimap_mask","L","B",0.135,0.12,0.063,0.134)
+		SetMinimapComponentPosition("minimap_blur","L","B",0.0,0.0,0.185,0.250)
+
+		SetBigmapActive(true,false)
+
+		Wait(5000)
+
+		SetBigmapActive(false,false)
 	end
 end)
