69555ae46c56a7a322c4758116368f32cb12bfd1
PauladaHR <pemeduardo1@hotmail.com>
2022-11-04 21:32:17 -0300

    Update Oxmysql 2.5.2

---
+++ resources/logs/entity.lua
@@ -0,0 +1,195 @@
+Model: 1114264700
+Coords: 822.33,-2149.02,29.58
+Rotation: 0.0,0.0,0.0
+Heading: 360.0
+
+Model: 1885233650
+Coords: 967.21,-2105.42,31.49
+Rotation: 1.7,-4.04,39.11
+Heading: 39.1
+
+Model: 1885233650
+Coords: 994.85,-2180.6,29.84
+Rotation: -23.48,39.33,124.46
+Heading: 138.75
+
+Model: 1885233650
+Coords: 997.58,-2182.57,29.99
+Rotation: -10.2,-6.07,11.35
+Heading: 11.42
+
+Model: 1885233650
+Coords: 408.61,-1007.71,29.27
+Rotation: 0.0,0.0,-128.7
+Heading: 231.3
+
+Model: 1885233650
+Coords: 408.6,-1007.72,29.27
+Rotation: 0.0,0.0,-128.65
+Heading: 231.35
+
+Model: 1885233650
+Coords: 409.14,-1007.53,29.27
+Rotation: 0.0,0.0,-126.63
+Heading: 233.37
+
+Model: 1885233650
+Coords: 415.47,-988.89,29.38
+Rotation: 0.0,0.0,-86.6
+Heading: 273.4
+
+Model: 1885233650
+Coords: 415.47,-988.91,28.76
+Rotation: 0.0,0.0,-86.09
+Heading: 273.91
+
+Model: 1885233650
+Coords: 415.43,-988.82,27.53
+Rotation: -0.04,0.01,-85.12
+Heading: 274.88
+
+Model: 1885233650
+Coords: 415.26,-989.08,29.37
+Rotation: 9.1,1.38,-24.49
+Heading: 335.2
+
+Model: 1885233650
+Coords: 415.11,-989.29,29.42
+Rotation: 0.0,0.0,10.28
+Heading: 10.28
+
+Model: 1885233650
+Coords: 415.39,-988.94,29.42
+Rotation: 0.0,0.01,11.34
+Heading: 11.34
+
+Model: 1885233650
+Coords: 416.98,-989.93,29.37
+Rotation: 8.06,-0.81,76.57
+Heading: 76.8
+
+Model: 1885233650
+Coords: 416.93,-989.83,29.39
+Rotation: 0.0,0.0,86.18
+Heading: 86.18
+
+Model: 1885233650
+Coords: 417.3,-990.31,29.37
+Rotation: 0.01,0.0,85.04
+Heading: 85.04
+
+Model: 1885233650
+Coords: 416.73,-989.62,29.39
+Rotation: 0.0,0.0,86.18
+Heading: 86.18
+
+Model: 1885233650
+Coords: 415.7,-988.97,29.41
+Rotation: 0.0,0.0,-116.62
+Heading: 243.38
+
+Model: 1885233650
+Coords: 415.71,-988.97,29.42
+Rotation: 0.0,0.0,-111.5
+Heading: 248.5
+
+Model: 1885233650
+Coords: 415.69,-988.98,29.46
+Rotation: 0.0,0.0,-110.94
+Heading: 249.06
+
+Model: 1885233650
+Coords: 415.71,-988.95,29.09
+Rotation: 0.0,0.0,-116.22
+Heading: 243.78
+
+Model: 1885233650
+Coords: 415.99,-988.6,29.14
+Rotation: 50.51,-20.05,-40.74
+Heading: 316.88
+
+Model: 1885233650
+Coords: 416.25,-988.39,29.46
+Rotation: 0.0,0.0,2.16
+Heading: 2.16
+
+Model: 1885233650
+Coords: 415.82,-988.57,29.42
+Rotation: 0.01,0.01,2.93
+Heading: 2.93
+
+Model: 1885233650
+Coords: 416.18,-988.43,29.41
+Rotation: 0.0,0.0,8.51
+Heading: 8.51
+
+Model: 1885233650
+Coords: 416.19,-988.04,28.77
+Rotation: 0.0,0.0,8.51
+Heading: 8.51
+
+Model: 1885233650
+Coords: 416.64,-987.63,28.77
+Rotation: 0.0,0.0,8.51
+Heading: 8.51
+
+Model: 1885233650
+Coords: 418.07,-994.88,29.39
+Rotation: 0.0,0.0,-179.42
+Heading: 180.58
+
+Model: 1885233650
+Coords: 416.88,-996.28,29.31
+Rotation: 0.01,0.01,-170.0
+Heading: 190.0
+
+Model: 1885233650
+Coords: 417.36,-993.66,29.34
+Rotation: 0.0,0.0,-131.21
+Heading: 228.79
+
+Model: 1885233650
+Coords: 417.33,-993.92,29.33
+Rotation: 0.0,0.0,-131.19
+Heading: 228.81
+
+Model: 1885233650
+Coords: 416.13,-993.48,28.47
+Rotation: 0.0,0.0,117.29
+Heading: 117.29
+
+Model: 1885233650
+Coords: 415.95,-993.34,29.37
+Rotation: 0.0,0.0,150.05
+Heading: 150.05
+
+Model: 1885233650
+Coords: 416.05,-995.83,29.34
+Rotation: 0.0,0.01,36.86
+Heading: 36.86
+
+Model: 1885233650
+Coords: 416.15,-995.49,28.59
+Rotation: 0.0,0.0,36.86
+Heading: 36.86
+
+Model: -331467772
+Coords: -253.73,-690.69,32.98
+Rotation: 0.37,0.4,125.69
+Heading: 125.69
+
+Model: -331467772
+Coords: -253.75,-690.66,32.97
+Rotation: 0.77,1.08,126.64
+Heading: 126.64
+
+Model: -331467772
+Coords: -253.75,-690.66,32.97
+Rotation: 0.8,1.05,126.71
+Heading: 126.7
+
+Model: -331467772
+Coords: -253.75,-690.66,32.96
+Rotation: 0.41,1.61,126.63
+Heading: 126.64
+
